7e2a6dffe86bdf706ddcc06c4f0713a4f3702802
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-03-01 21:36:40 -0300

    feat: create first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as s from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <s.Wrapper>
+    <s.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <s.Title>React Avançado</s.Title>
+    <s.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </s.Description>
+    <s.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para tela do computador código"
+    />
+  </s.Wrapper>
 )
 
 export default Main
