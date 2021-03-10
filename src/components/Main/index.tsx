5c509e4d1786fc28bf0fa2bae7c3a015278c3a3f
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-03-10 09:45:05 -0300

    feat: add storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,21 @@
-import * as s from './styles'
+import * as S from './styles'
 
-const Main = () => (
-  <s.Wrapper>
-    <s.Logo
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) => (
+  <S.Wrapper>
+    <S.Logo
       src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <s.Title>React Avançado</s.Title>
-    <s.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </s.Description>
-    <s.Illustration
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
       src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para tela do computador código"
+      alt="Um desenvolvedor de frente para uma tela com código."
     />
-  </s.Wrapper>
+  </S.Wrapper>
 )
 
 export default Main
